83fbb914e49c439ab695cae7988c32cde97729af
Stefan Tammer <st.tammer@gmail.com>
2024-09-29 17:41:37 +0200

    version 1.0.0, remove wgx and egui

--- src/lib.rs
+++ src/lib.rs
@@ -23,10 +23,6 @@ pub mod timer;
 #[cfg(feature = "rng")]
 pub mod rng;
 
-// wgx
-#[cfg(feature = "wgx")]
-pub use wgx;
-
 // icon loader
 #[cfg(feature = "icon_loader")]
 #[cfg(target_os = "linux")]
@@ -35,21 +31,3 @@ pub mod icon_loader;
 // wake_lock
 #[cfg(feature = "wake_lock")]
 pub mod wake_lock;
-
-// egui
-#[cfg(any(feature = "epaint", feature = "egui"))]
-#[path="."]
-pub mod egui {
-
-    pub use epaint::{self, ecolor, emath};
-    pub use egui_wgpu;
-
-    #[cfg(feature = "egui")]
-    pub use egui::*;
-
-    #[cfg(feature = "egui")]
-    pub use egui_winit;
-
-    mod egui_ctx;
-    pub use egui_ctx::*;
-}
